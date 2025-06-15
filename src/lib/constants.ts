5216797f78bd4e23f950f302ab4bf777e2e6945b
salmonco <xkqjsslsek80@naver.com>
2025-06-15 14:49:03 +0900

    feat: Add constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,12 @@
+// TODO: service title 변경
 export const SERVICE_TITLE = "ShadcnUI/React";
 
+// TODO: github url 변경
 export const GITHUB_URL =
   "https://github.com/leoMirandaa/shadcn-landing-page.git";
+
+// TODO: contact email 변경
+export const CONTACT_EMAIL = "abc@def.com";
+
+// TODO: demo url 변경
+export const DEMO_URL = "https://shadcn-landing-page.vercel.app";
